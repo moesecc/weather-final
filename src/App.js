7776b12e46810d797afa4e5fe74059e9c7b806b7
mhmdnab <mhmdnab004@gmail.com>
2023-06-12 21:14:18 +0300

    styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("Beirut");
+  const [weatherData, setWeatherData] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get(
+      `https://api.weatherapi.com/v1/current.json?key=7b96e15176bf400498a181557230406&q=${city}`
+    );
+    setWeatherData(data);
+    console.log(data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          placeholder="City"
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <button type="submit">Search</button>
+        {weatherData && (
+          <>
+            <h1 className="display">This is {weatherData.location.country}</h1>
+            <fieldset className="set">
+              <h2>Temperature: {weatherData.current.temp_c} C</h2>
+              <h2>Feels Like: {weatherData.current.feelslike_c} C</h2>
+            </fieldset>
+            <div className="current">
+              <h1>Current Weather Details</h1>
+              <p className="p">
+                <img alt="condition" src={weatherData.current.condition.icon} />
+                <p>{weatherData.current.condition.text}</p>
+              </p>
+            </div>
+          </>
+        )}
+      </form>
     </div>
   );
 }
